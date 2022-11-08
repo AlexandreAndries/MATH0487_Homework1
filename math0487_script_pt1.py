1f5de171812b8422333ba274eba60e2d382017d6
AlexAndries <alexandreandries23@gmail.com>
2022-11-08 23:25:59 +0000

    Q1.2 (almost done)

--- math0487_script_pt1.py
+++ math0487_script_pt1.py
@@ -1,18 +1,71 @@
 import pandas as pd
+from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+
+def plot_diagram(df, title, ylab):
+
+    fig, axes = plt.subplots(1)
+
+    axes.set_title(title)
+    axes.set(ylabel=ylab)
+    axes.set_facecolor('azure')  # --> light blue background
+
+    df.plot(kind='box')
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_histogram(df, title, ylab, xlab):
+    fig, axes = plt.subplots(1)
+
+    axes.set_title(title)
+    axes.set(ylabel=ylab, xlabel=xlab)
+    axes.set_facecolor('azure')  # --> light blue background
+
+    plt.hist(df)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_cdf(df, title, xlab):
+    fig, axes = plt.subplots(1)
+
+    # Frequency
+    df = pd.DataFrame(df)
+    df.rename(columns= {title:'value'}, inplace = True)
+    dfFreq = df.sort_values(by=['value'])
+    dfFreq = df.groupby('value')['value'].agg('count').pipe(pd.DataFrame).rename(columns={'value' : 'frequency'})
+
+    # PDF
+    dfFreq['pdf'] = dfFreq['frequency'] / sum(dfFreq['frequency'])
+
+    # CDF
+    dfFreq['cdf'] = dfFreq['pdf'].cumsum()
+    dfFreq = dfFreq.reset_index()
+
+    plt.plot(dfFreq['value'],dfFreq['cdf'], 'r')
+
+    axes.set_title(title + " CDF")
+    axes.set(xlabel=xlab, ylabel='Probability')
+    axes.set_facecolor('azure')
+    plt.tight_layout()
+    plt.show()
+
+
 
 # Q1.1 =========================================================================
 # initialisation de la dataframe (à remplir)
 idx = ['10th', '20th', '30th']
 var = ['fixed acidity', 'pH', 'alcohol']
-df_q1_1 = pd.DataFrame(np.zeros((3,3)), idx, var)
+df_q1_1 = pd.DataFrame(np.zeros((3, 3)), idx, var)
 df_q1_1
 
 # chargement du dataset
-data = pd.read_csv('data/math0487_fa22_hw1_data.csv') #
-df_q1_1 = pd.DataFrame(data, index=[9,19,29], columns=var) #
+data = pd.read_csv('data/math0487_fa22_hw1_data.csv')
+df_q1_1 = pd.DataFrame(data, index=[9, 19, 29], columns=var)
 df_q1_1.index = idx
 
 # calcul des valeurs demandées
@@ -26,16 +79,39 @@ medians = df_q1_1.median()
 stdDevs = df_q1_1.std()
 
 # visualisation de la dataframe
-print(df_q1_1) #
+print(df_q1_1)
 
 # Q1.2 =========================================================================
 # initialisation de la dataframe (à remplir)
 var = ['fixed acidity', 'pH', 'alcohol']
 idx = ['mean', 'std', 'median', 'q25', 'q75']
-df_q1_2 = pd.DataFrame(np.zeros((5,3)), idx, var)
+df_q1_2 = pd.DataFrame(np.zeros((5, 3)), idx, var)
 
 # calcul des valeurs demandées
-...
+fullDF = pd.DataFrame(data, columns=var)
+df_q1_2.loc["mean"] = fullDF.mean()
+df_q1_2.loc["std"] = fullDF.std()
+df_q1_2.loc["median"] = fullDF.median()
+df_q1_2.loc["q25"] = fullDF.quantile(q=0.25)
+df_q1_2.loc["q75"] = fullDF.quantile(q=0.75)
 
-# visualisation de la dataframe
+# # visualisation de la dataframe
 print(df_q1_2)
+
+# boites à moustaches
+plot_diagram(fullDF['fixed acidity'], 'fixed acidity boxplot', 'g/dm^3')
+plot_diagram(fullDF['pH'], 'pH boxplot', '-')
+plot_diagram(fullDF['alcohol'], 'alcohol boxplot', 'vol%')
+
+# histogrammes
+plot_histogram(fullDF['fixed acidity'],
+               'fixed acidity histogram', 'effectifs', 'g/dm^3')
+plot_histogram(fullDF['pH'], 'pH histogram', 'effectifs', '-')
+plot_histogram(fullDF['alcohol'], 'alcohol histogram', 'effectifs', 'vol%')
+
+# cdf
+plot_cdf(fullDF['fixed acidity'],
+               'fixed acidity', 'g/dm^3')
+plot_cdf(fullDF['pH'], 'pH', '-')
+plot_cdf(fullDF['alcohol'], 'alcohol', 'vol%')
+# Q1.3 =========================================================================
